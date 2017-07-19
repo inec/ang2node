4e7d543d5aca301eab6cc995cd2315e0435636f1
YC Lu <luyeg+git>
2017-07-19 00:20:35 -0700

    'ts'

--- src/app/messages-component.ts
+++ src/app/messages-component.ts
@@ -2,6 +2,6 @@ import { Component } from '@angular/core'
 
 @Component({
     selector: 'messages',
-    template: 'this is the messages component'
+    template: 'this is the messages component based Twg'
 })
 export class MessagesComponent {}
