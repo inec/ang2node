7b068f4ea337e05bd15aec62ad1de9bf72f5e25d
YC Lu <luyeg+git>
2017-07-20 00:01:52 -0700

    tt fail

--- src/app/messages-component.ts
+++ src/app/messages-component.ts
@@ -2,7 +2,9 @@ import { Component } from '@angular/core'
 
 @Component({
     selector: 'messages',
-    template: 'this is the messages component <div *ngFor="let message of messages">{{message.text}} by {{message.owner}}</div>'
+    template: 'test<div *ngFor="let message of messages">            {{message.text}} by {{message.owner}}        </div>'
+   
+       
 })
 export class MessagesComponent {
     messages = [{text:'some tesxt', owner: 'CHIchi'},{text:'some stesxt', owner: ' C2cdhi'}];
