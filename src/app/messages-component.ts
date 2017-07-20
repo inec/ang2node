1670c5c2f297737cc478df367b87c2b5063d0ec2
YC Lu <luyeg+git>
2017-07-19 23:07:05 -0700

    'for'

--- src/app/messages-component.ts
+++ src/app/messages-component.ts
@@ -2,9 +2,9 @@ import { Component } from '@angular/core'
 
 @Component({
     selector: 'messages',
-    template: 'this is the messages component based Twg {{messages[0].owner}}'
+    template: 'this is the messages component <div *ngFor="let message of messages">{{message.text}} by {{message.owner}}</div>'
 })
 export class MessagesComponent {
-    messages = [{text:'some tesxt', owner: 'CHIchi'},{text:'some stesxt', owner: 'cdhi'}];
+    messages = [{text:'some tesxt', owner: 'CHIchi'},{text:'some stesxt', owner: ' C2cdhi'}];
 
 }
