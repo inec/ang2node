3be0932e8d9a092f7a892b47b86f23e9777b0123
YC Lu <luyeg+git>
2017-07-19 22:42:04 -0700

    '2'

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
-
+import { MessagesComponent } from './messages-component';
 @Component({
   selector: 'my-app',
-  template: `<h1>Hi Monde {{name}}</h1>`,
+  template: `<h1>Hi Monde  w {{name}}</h1> <messages></messages>`,
 })
 export class AppComponent  { name = 'World'; }
