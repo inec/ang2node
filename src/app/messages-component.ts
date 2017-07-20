9c1dbad276c83472bffbd90cd054d8398fdb80e9
YC Lu <luyeg+git>
2017-07-19 22:48:07 -0700

    't'

--- src/app/messages-component.ts
+++ src/app/messages-component.ts
@@ -2,9 +2,9 @@ import { Component } from '@angular/core'
 
 @Component({
     selector: 'messages',
-    template: 'this is the messages component based Twg {{messages.length}}'
+    template: 'this is the messages component based Twg {{messages[0].owner}}'
 })
 export class MessagesComponent {
-    messages = [{text:'some tesxt', owner: 'chi'},{text:'some stesxt', owner: 'cdhi'}];
+    messages = [{text:'some tesxt', owner: 'CHIchi'},{text:'some stesxt', owner: 'cdhi'}];
 
 }
